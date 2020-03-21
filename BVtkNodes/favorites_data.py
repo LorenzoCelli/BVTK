a3ae0ae9b3253ff736d36f5c4b911a0eb1107606
lorenzocelli <celli.lorenzo@yahoo.it>
2020-03-21 19:23:24 +0100

    Complete reset

--- BVtkNodes/favorites_data.py
+++
@@ -1,6 +0,0 @@
-favorites = [('VTK2BlenderType', 'ToBlender'),
- ('VTKInfoNodeType', 'Info'),
- ('VTKConeSourceType', 'vtkConeSource'),
- ('VTKClipPolyDataType', 'vtkClipPolyData'),
- ('VTKColorMapperType', 'ColorMapper'),
- ('VTKColorMapType', 'ColorMap')]
